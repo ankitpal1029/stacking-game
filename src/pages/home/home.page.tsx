2c50bf39f37b99e8ba83a86352d136fad2cc2569
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-03-02 00:56:58 +0530

    basic layout

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Post} from '../../common/interface/post.interface';
+import PostsGrid from '../../components/posts-grid/posts-grid.component';
 
 const Home: React.FC = () => {
+    const posts: Post[] = [
+        {
+            id:'1',
+            title: 'Post 1',
+            body: 'body 1'
+        },
+
+        {
+            id:'2',
+            title: 'Post 2',
+            body: 'body 2'
+        }
+    ]
     return (
-        <div className="home">Home</div>
+        <div className="home">
+            <PostsGrid posts={posts}/>
+
+        </div>
     )
 }
 

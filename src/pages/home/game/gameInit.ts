7b4dce7cb0fe8feb457238d907bce556adf201e1
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-03-04 01:37:13 +0530

    FEAT: Full Physics integration with game

--- src/pages/home/game/gameInit.ts
+++ src/pages/home/game/gameInit.ts
@@ -1,16 +1,22 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon';
 
 class Game{
     private scene: THREE.Scene;
     private renderer: THREE.WebGLRenderer;
     private camera: THREE.OrthographicCamera;
     private originalBoxSize: number;
+    private world: CANNON.World;
 
     private stack: any[] = [];
     private boxHeight: number = 1;
+    private overhangs: any[] = [];
     constructor(){
         this.originalBoxSize = 3;
         this.scene = new THREE.Scene();
+        this.world = new CANNON.World();
+        this.world.gravity.set(0, -10, 0);
+        this.world.solver.iterations = 40;
 
         this.addLayer(0, 0, this.originalBoxSize, this.originalBoxSize, 'a');
         this.addLayer(-10, 0, this.originalBoxSize, this.originalBoxSize, 'x');
@@ -41,15 +47,23 @@ class Game{
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         
     }
+    cannonReturner(){
+        return ({
+            world:this.world,
+            overhangs:this.overhangs
+        }
+        );
+    }
+
 
     addLayer(x:number, z:number, width:number, depth:number, direction:string){
         const y = this.boxHeight * this.stack.length; // Add the new box one layer higher
-        const layer:any = this.generateBox(x, y, z, width, depth);
+        const layer:any = this.generateBox(x, y, z, width, depth, false);
         layer.direction = direction;
         this.stack.push(layer);
     }
 
-    generateBox(x:number, y:number,z:number, width:number, depth:number){
+    generateBox(x:number, y:number,z:number, width:number, depth:number, falls: boolean){
         const geometry = new THREE.BoxGeometry(width, this.boxHeight, depth);
 
         const color = new THREE.Color(`hsl(${30 + this.stack.length * 4}, 100%, 50%)`);
@@ -61,12 +75,27 @@ class Game{
         
         this.scene.add(mesh);
 
+        const shape = new CANNON.Box(
+            new CANNON.Vec3(width /2, this.boxHeight /2, depth/2)
+        )
+        let mass = falls? 5: 0;
+        const body = new CANNON.Body({mass, shape});
+        body.position.set(x, y, z);
+        this.world.addBody(body);
+
         return{
             threejs: mesh,
+            cannonjs: body,
             width,
             depth
         }
 
+        }
+
+        addOverhang(x:number, z:number, width:number, depth:number){
+            const y = this.boxHeight * (this.stack.length - 1);
+            const overhang = this.generateBox(x, y, z, width, depth, true)
+            this.overhangs.push(overhang);
         }
     render(){
         return({
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,34 +1,69 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Game from './game/gameInit';
+import * as CANNON from 'cannon';
 
-class Home extends Component<{},{gameStarted:boolean}>{
+class Home extends Component<{},{gameStarted:boolean, gameEnded:boolean, score:number}>{
 
     private gameContainer: any;
     private boxHeight:number = 1;
-    private originalBoxSize: number = 3;
 
     constructor(props:any){
         super(props);
         this.gameContainer = React.createRef();
         this.state = {
-            gameStarted:false
+            gameStarted:false,
+            gameEnded:false,
+            score:0
+
         }
+
+    }
+    cutBox(topLayer: any, overlap: number, size:number, delta: number){
+        const direction = topLayer.direction;
+        const newWidth = direction === "x"? overlap: topLayer.width;
+        const newDepth = direction === "z"? overlap: topLayer.depth;
+
+        topLayer.width = newWidth;
+        topLayer.depth = newDepth;
+
+        topLayer.threejs.scale[direction] = overlap /size;
+        topLayer.threejs.position[direction] -= delta/2;
+
+        topLayer.cannonjs.position[direction] -= delta/2;
+
+        const shape = new CANNON.Box(
+            new CANNON.Vec3(newWidth/2, this.boxHeight, newDepth/2)
+        );
+
+        topLayer.cannonjs.shapes = [];
+        topLayer.cannonjs.addShape(shape);
+    }
+
+    updatePhysics(world: CANNON.World, overhangs: any[]){
+        world.step(1/60);
+        overhangs.forEach((element) => {
+            element.threejs.position.copy(element.cannonjs.position);
+            element.threejs.quaternion.copy(element.cannonjs.quaternion);
+        })
+
 
     }
 
 
-    animate(scene:THREE.Scene, camera:THREE.OrthographicCamera, stack: any[], renderer:THREE.WebGLRenderer) {
+    animate(scene:THREE.Scene, camera:THREE.OrthographicCamera, stack: any[], renderer:THREE.WebGLRenderer, world: CANNON.World,overhangs:any[]) {
         const speed = 0.15;
 
         const topLayer = stack[stack.length - 1];
         console.log(topLayer);
         topLayer.threejs.position[topLayer.direction] += speed;
+        topLayer.cannonjs.position[topLayer.direction] += speed;
 
         if(camera.position.y < this.boxHeight * (stack.length - 2) + 4){
             camera.position.y += speed;
 
         }
+        this.updatePhysics(world,overhangs);
 
         return renderer.render( scene, camera );
     };
@@ -41,21 +76,84 @@ class Home extends Component<{},{gameStarted:boolean}>{
         window.addEventListener("click", () => {
             if(!this.state.gameStarted){
                 console.log(stack);
-                renderer.setAnimationLoop(() => this.animate(scene, camera, stack, renderer));
+                let { world, overhangs } = gameRender.cannonReturner();
+                renderer.setAnimationLoop(() => this.animate(scene, camera, stack, renderer,world,overhangs ));
                 this.setState({
                     gameStarted:true
                 });
             }else{
                 const topLayer = stack[stack.length - 1];
+                const previousLayer = stack[stack.length - 2];
+
                 const direction = topLayer.direction;
 
-                const nextX = direction === "x"? 0: -10;
-                const nextZ = direction === "z"? 0: -10;
-                const newWidth = this.originalBoxSize;
-                const newDepth = this.originalBoxSize;
-                const nextDirection = direction === "x"?"z":"x";
+                const delta = topLayer.threejs.position[direction] - 
+                    previousLayer.threejs.position[direction];
 
-                gameRender.addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+                const overHangSize = Math.abs(delta);
+
+                const size = direction === "x"? topLayer.width: topLayer.depth;
+
+                const overlap = size - overHangSize;
+
+                if(overlap > 0){
+                    this.cutBox(topLayer, overlap, size, delta);
+
+                    const newWidth = direction === "x"? overlap: topLayer.width;
+                    const newDepth = direction === "z"? overlap: topLayer.depth;
+
+                    topLayer.width = newWidth;
+                    topLayer.depth = newDepth;
+
+                    topLayer.threejs.scale[direction] = overlap/size;
+                    topLayer.threejs.position[direction] = delta/2;
+
+                    const overHangShift = (overlap /2 + overHangSize /2) * Math.sign(delta);
+                    const overHangX = 
+                        direction === "x"
+                        ? topLayer.threejs.position.x + overHangShift
+                        : topLayer.threejs.position.x;
+
+                    const overHangZ = 
+                        direction === "z"
+                        ? topLayer.threejs.position.z + overHangShift
+                        : topLayer.threejs.position.z;
+
+                    const overHangWidth = direction === "x" ? overHangSize: newWidth;
+                    const overHangDepth = direction === "z" ? overHangSize: newDepth;
+                    
+                    // addOverhang
+                    gameRender.addOverhang(overHangX, overHangZ, overHangWidth, overHangDepth)
+
+
+
+                    const nextX = direction === "x"? topLayer.threejs.position.x: -10;
+                    const nextZ = direction === "z"? topLayer.threejs.position.z: -10;
+                    const nextDirection = direction === "x"?"z":"x";
+                    gameRender.addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+                    this.setState({
+                        score: this.state.score + 1
+                    })
+
+                }else{
+                    this.setState({
+                        gameEnded:true
+                    });
+                    gameRender.addOverhang(
+                        topLayer.threejs.position.x, 
+                        topLayer.threejs.position.z, 
+                        topLayer.width, 
+                        topLayer.depth
+                    )
+                    let { world, overhangs} = gameRender.cannonReturner();
+                    world.remove(topLayer.cannonjs);
+                    scene.remove(topLayer.threejs);
+
+                }
+
+
+
+
             }
         })
     
@@ -64,7 +162,12 @@ class Home extends Component<{},{gameStarted:boolean}>{
     
   render() {
     return (
-        <div ref={this.gameContainer}></div>
+        <div ref={this.gameContainer}>
+            <p>{this.state.score} </p>
+            {this.state.gameEnded && 
+                 <p>Games over</p>
+            }
+        </div>
         )
   }
   

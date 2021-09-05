0624796b09c53e974525ce629745e0c465144acc
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-09-05 13:20:48 +0530

    [SECURITY]: removed env from commit repository

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 const app = express();
@@ -10,9 +11,15 @@ const authRoutes = require('./routes/auth');
 app.use(express.json());
 app.use(cors());
 
+const { PORT, DBUSER, DBPASSWORD }= process.env;
+// connect to db
+mongoose.connect(`mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.e2cl6.mongodb.net/test`,() => {
+    console.log(`connected to db`);
+});
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
 app.use(authRoutes);
